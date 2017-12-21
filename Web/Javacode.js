c84295912e42a3129ebf11145d6ca5ae628ce796
Fabian Spescha <32870469+kazoom007@users.noreply.github.com>
2017-12-21 16:12:13 +0100

    renamed in javascipt

--- Web/Javacode.js
+++ Web/Javacode.js
@@ -1,11 +1,13 @@
 $(function(){
     d = new Date();
+
     day = d.getUTCDate();
     month = d.getUTCMonth();
+
   $("#dayOne").click(function(){
     if(day>=1){
       $("#dayOne").html("");
-      $("#dayOne").html("<img src='rsc\\jpg\\1._Turchen.jpg'/>");
+      $("#dayOne").html("<img src='rsc\\jpg\\1_Turchen.jpg'/>");
     }else {
       alert("Dieses Türchen kann erst in " + (1 - day) + " Tagen und "+ (12 - (month+1) )+" Monaten geöffnet werden!")
     }
@@ -13,7 +15,7 @@ $(function(){
   $("#dayTwo").click(function(){
     if(day>=2){
       $("#dayTwo").html("");
-      $("#dayTwo").html("<img src='rsc\\jpg\\2._Turchen.jpg'/>");
+      $("#dayTwo").html("<img src='rsc\\jpg\\2_Turchen.jpg'/>");
     }else {
       alert("Dieses Türchen kann erst in " + (2 - day) + " Tagen und "+ (12 - (month+1) )+" Monaten geöffnet geöffnet werden!")
     }
@@ -21,7 +23,7 @@ $(function(){
   $("#dayThree").click(function(){
     if(day>=3){
       $("#dayThree").html("");
-      $("#dayThree").html("<img src='rsc\\jpg\\3._Turchen.jpg'/>");
+      $("#dayThree").html("<img src='rsc\\jpg\\3_Turchen.jpg'/>");
     }else {
       alert("Dieses Türchen kann erst in " + (3 - day) + " Tagen und "+ (12 - (month+1) )+" Monaten geöffnet geöffnet werden!")
     }
@@ -29,7 +31,7 @@ $(function(){
   $("#dayFour").click(function(){
     if(day>=4){
       $("#dayFour").html("");
-      $("#dayFour").html("<img src='rsc\\jpg\\4._Turchen.jpg'/>");
+      $("#dayFour").html("<img src='rsc\\jpg\\4_Turchen.jpg'/>");
     }else {
       alert("Dieses Türchen kann erst in " + (4 - day) + " Tagen und "+ (12 - (month+1) )+" Monaten geöffnet geöffnet werden!")
     }
@@ -37,7 +39,7 @@ $(function(){
   $("#dayFive").click(function(){
     if(day>=5){
       $("#dayFive").html("");
-      $("#dayFive").html("<img src='rsc\\jpg\\5._Turchen.jpg'/>");
+      $("#dayFive").html("<img src='rsc\\jpg\\5_Turchen.jpg'/>");
     }else {
       alert("Dieses Türchen kann erst in " + (5 - day) + " Tagen und "+ (12 - (month+1) )+" Monaten geöffnet geöffnet werden!")
     }
@@ -45,7 +47,7 @@ $(function(){
   $("#daySix").click(function(){
     if(day>=6){
       $("#daySix").html("");
-      $("#daySix").html("<img src='rsc\\jpg\\6._Turchen.jpg'/>");
+      $("#daySix").html("<img src='rsc\\jpg\\6_Turchen.jpg'/>");
     }else {
       alert("Dieses Türchen kann erst in " + (6 - day) + " Tagen und "+ (12 - (month+1) )+" Monaten geöffnet geöffnet werden!")
     }
@@ -53,7 +55,7 @@ $(function(){
   $("#daySeven").click(function(){
     if(day>=7){
       $("#daySeven").html("");
-      $("#daySeven").html("<img src='rsc\\jpg\\7._Turchen.jpg'/>");
+      $("#daySeven").html("<img src='rsc\\jpg\\7_Turchen.jpg'/>");
     }else {
       alert("Dieses Türchen kann erst in " + (7 - day) + " Tagen und "+ (12 - (month+1) )+" Monaten geöffnet geöffnet werden!")
     }
@@ -61,7 +63,7 @@ $(function(){
   $("#dayEight").click(function(){
     if(day>=8){
       $("#dayEight").html("");
-      $("#dayEight").html("<img src='rsc\\jpg\\8._Turchen.jpg'/>");
+      $("#dayEight").html("<img src='rsc\\jpg\\8_Turchen.jpg'/>");
     }else {
       alert("Dieses Türchen kann erst in " + (8 - day) + " Tagen und "+ (12 - (month+1) )+" Monaten geöffnet geöffnet werden!")
     }
@@ -69,7 +71,7 @@ $(function(){
   $("#dayNine").click(function(){
     if(day>=9){
       $("#dayNine").html("");
-      $("#dayNine").html("<img src='rsc\\jpg\\9._Turchen.jpg'/>");
+      $("#dayNine").html("<img src='rsc\\jpg\\9_Turchen.jpg'/>");
     }else {
       alert("Dieses Türchen kann erst in " + (9 - day) + " Tagen und "+ (12 - (month+1) )+" Monaten geöffnet geöffnet werden!")
     }
@@ -77,7 +79,7 @@ $(function(){
   $("#dayTen").click(function(){
     if(day>=10){
       $("#dayTen").html("");
-      $("#dayTen").html("<img src='rsc\\jpg\\10._Turchen.jpg'/>");
+      $("#dayTen").html("<img src='rsc\\jpg\\10_Turchen.jpg'/>");
     }else {
       alert("Dieses Türchen kann erst in " + (10 - day) + " Tagen und "+ (12 - (month+1) ) +" Monaten geöffnet geöffnet werden!")
     }
@@ -85,7 +87,7 @@ $(function(){
   $("#dayEleven").click(function(){
     if(day>=11){
       $("#dayEleven").html("");
-      $("#dayEleven").html("<img src='rsc\\jpg\\11._Turchen.jpg'/>");
+      $("#dayEleven").html("<img src='rsc\\jpg\\11_Turchen.jpg'/>");
     }else {
       alert("Dieses Türchen kann erst in " + (11 - day) + " Tagen und "+ (12 - (month+1) )+" Monaten geöffnet geöffnet werden!")
     }
@@ -93,7 +95,7 @@ $(function(){
   $("#dayTwelve").click(function(){
     if(day>=12){
       $("#dayTwelve").html("");
-      $("#dayTwelve").html("<img src='rsc\\jpg\\12._Turchen.jpg'/>");
+      $("#dayTwelve").html("<img src='rsc\\jpg\\12_Turchen.jpg'/>");
     }else {
       alert("Dieses Türchen kann erst in " + (12 - day) + " Tagen und "+ (12 - (month+1) )+" Monaten geöffnet geöffnet werden!")
     }
@@ -101,7 +103,7 @@ $(function(){
   $("#dayThirteen").click(function(){
     if(day>=13){
       $("#dayThirteen").html("");
-      $("#dayThirteen").html("<img src='rsc\\jpg\\13._Turchen.jpg'/>");
+      $("#dayThirteen").html("<img src='rsc\\jpg\\13_Turchen.jpg'/>");
     }else {
       alert("Dieses Türchen kann erst in " + (13 - day) + " Tagen und "+ (12 - (month+1) )+" Monaten geöffnet geöffnet werden!")
     }
@@ -109,7 +111,7 @@ $(function(){
   $("#dayFourteen").click(function(){
     if(day>=14){
       $("#dayFourteen").html("");
-      $("#dayFourteen").html("<img src='rsc\\jpg\\14._Turchen.jpg'/>");
+      $("#dayFourteen").html("<img src='rsc\\jpg\\14_Turchen.jpg'/>");
     }else {
       alert("Dieses Türchen kann erst in " + (14 - day) + " Tagen und "+ (12 - (month+1) )+" Monaten geöffnet geöffnet werden!")
     }
@@ -117,7 +119,7 @@ $(function(){
   $("#dayFifteen").click(function(){
     if(day>=15){
       $("#dayFifteen").html("");
-      $("#dayFifteen").html("<img src='rsc\\jpg\\15._Turchen.jpg'/>");
+      $("#dayFifteen").html("<img src='rsc\\jpg\\15_Turchen.jpg'/>");
     }else {
       alert("Dieses Türchen kann erst in " + (15 - day) + " Tagen und "+ (12 - (month+1) )+" Monaten geöffnet geöffnet werden!")
     }
@@ -125,7 +127,7 @@ $(function(){
   $("#daySixteen").click(function(){
     if(day>=16){
       $("#daySixteen").html("");
-      $("#daySixteen").html("<img src='rsc\\jpg\\16._Turchen.jpg'/>");
+      $("#daySixteen").html("<img src='rsc\\jpg\\16_Turchen.jpg'/>");
     }else {
       alert("Dieses Türchen kann erst in " + (16 - day) + " Tagen und "+ (12 - (month+1) )+" Monaten geöffnet geöffnet werden!")
     }
@@ -133,7 +135,7 @@ $(function(){
   $("#daySeventeen").click(function(){
     if(day>=17){
       $("#daySeventeen").html("");
-      $("#daySeventeen").html("<img src='rsc\\jpg\\17._Turchen.jpg'/>");
+      $("#daySeventeen").html("<img src='rsc\\jpg\\17_Turchen.jpg'/>");
     }else {
       alert("Dieses Türchen kann erst in " + (17 - day) + " Tagen und "+ (12 - (month+1) )+" Monaten geöffnet geöffnet werden!")
     }
@@ -141,7 +143,7 @@ $(function(){
   $("#dayEighteen").click(function(){
     if(day>=18){
       $("#dayEighteen").html("");
-      $("#dayEighteen").html("<img src='rsc\\jpg\\18._Turchen.jpg'/>");
+      $("#dayEighteen").html("<img src='rsc\\jpg\\18_Turchen.jpg'/>");
     }else {
       alert("Dieses Türchen kann erst in " + (18 - day) + " Tagen und "+ (12 - (month+1) )+" Monaten geöffnet geöffnet werden!")
     }
@@ -149,7 +151,7 @@ $(function(){
   $("#dayNineteen").click(function(){
     if(day>=19){
       $("#dayNineteen").html("");
-      $("#dayNineteen").html("<img src='rsc\\jpg\\19._Turchen.jpg'/>");
+      $("#dayNineteen").html("<img src='rsc\\jpg\\19_Turchen.jpg'/>");
     }else {
       alert("Dieses Türchen kann erst in " + (19 - day) + " Tagen und "+ (12 - (month+1) )+" Monaten geöffnet geöffnet werden!")
     }
@@ -157,7 +159,7 @@ $(function(){
   $("#dayTwenty").click(function(){
     if(day>=20){
       $("#dayTwenty").html("");
-      $("#dayTwenty").html("<img src='rsc\\jpg\\20._Turchen.jpg'/>");
+      $("#dayTwenty").html("<img src='rsc\\jpg\\20_Turchen.jpg'/>");
     }else {
       alert("Dieses Türchen kann erst in " + (20 - day) + " Tagen und "+ (12 - (month+1) )+" Monaten geöffnet geöffnet werden!")
     }
@@ -165,7 +167,7 @@ $(function(){
   $("#dayTwentyone").click(function(){
     if(day>=21){
       $("#dayTwentyone").html("");
-      $("#dayTwentyone").html("<img src='rsc\\jpg\\21._Turchen.jpg'/>");
+      $("#dayTwentyone").html("<img src='rsc\\jpg\\21_Turchen.jpg'/>");
     }else {
       alert("Dieses Türchen kann erst in " + (21 - day) + " Tagen und "+ (12 - (month+1) )+" Monaten geöffnet geöffnet werden!")
     }
@@ -173,7 +175,7 @@ $(function(){
   $("#dayTwentytwo").click(function(){
     if(day>=22){
       $("#dayTwentytwo").html("");
-      $("#dayTwentytwo").html("<img src='rsc\\jpg\\22._Turchen.jpg'/>");
+      $("#dayTwentytwo").html("<img src='rsc\\jpg\\22_Turchen.jpg'/>");
     }else {
       alert("Dieses Türchen kann erst in " + (22 - day) + " Tagen und "+ (12 - (month+1) )+" Monaten geöffnet geöffnet werden!")
     }
@@ -181,7 +183,7 @@ $(function(){
   $("#dayTwentythree").click(function(){
     if(day>=23){
       $("#dayTwentythree").html("");
-      $("#dayTwentythree").html("<img src='rsc\\jpg\\23._Turchen.jpg'/>");
+      $("#dayTwentythree").html("<img src='rsc\\jpg\\23_Turchen.jpg'/>");
     }else {
       alert("Dieses Türchen kann erst in " + (23 - day) + " Tagen und "+ (12 - (month+1) )+" Monaten geöffnet geöffnet werden!")
     }
@@ -189,7 +191,7 @@ $(function(){
   $("#dayTwentyfour").click(function(){
     if(day>=24){
       $("#dayTwentyfour").html("");
-      $("#dayTwentyfour").html("<img src='rsc\\jpg\\24._Turchen.jpg'/>");
+      $("#dayTwentyfour").html("<img src='rsc\\jpg\\24_Turchen.jpg'/>");
     }else {
       alert("Dieses Türchen kann erst in " + (24 - day) + " Tagen und "+ (12 - (month+1) )+" Monaten geöffnet geöffnet werden!")
     }
